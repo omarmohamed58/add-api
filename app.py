1a1f8d07c85ba222a5e04080d2f9acb4dfb4f7e1
omarmohamed58 <137194688+omarmohamed58@users.noreply.github.com>
2024-02-09 16:12:09 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/add', methods=['POST'])
+
+def add():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 + num2
+    return jsonify({'result': result})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
